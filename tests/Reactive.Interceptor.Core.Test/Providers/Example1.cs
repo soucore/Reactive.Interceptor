09e9d2bfa7720e0996c3dbcec21b44947e1a3d0a
Bruno Moura <contato@brunomoura.dev>
2022-06-27 19:06:08 -0300

    Test builder to provider reflection

---
+++ tests/Reactive.Interceptor.Core.Test/Providers/Example1.cs
@@ -0,0 +1,21 @@
+﻿using Reactive.Interceptor.Providers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactive.Interceptor.Core.Test.Providers
+{
+    public class Example1 : ProviderBase
+    {
+        public override string ProviderName => throw new System.NotImplementedException();
+
+        public override Task ExecuteSourceAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Task<Response> Sink(object data)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
